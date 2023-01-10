78b8324133535b6fc4e3d131bd91ae53266a8cf1
alxfo <120241709+alxfo@users.noreply.github.com>
2023-01-10 21:17:50 +0800

    Improve readability by using full words for selections

--- javascript.js
+++ javascript.js
@@ -1,30 +1,24 @@
 // save options array to variable
-const options = ['r', 'p', 's']
+const options = ['rock', 'paper', 'scissors']
 
 // randomly generate computer choice
 let computerSelection = options[Math.floor(Math.random() * options.length)];
-console.log(computerSelection);
 
 // prompt user for input
 let playerSelection = prompt('Please choose: Rock, Paper or Scissors?');
-console.log(playerSelection);
 
 // make playerSelection lower case
 playerSelection = playerSelection.toLowerCase();
-
-// take first letter from playerSelection
-playerSelection = playerSelection[0]
-console.log(playerSelection);
 
 // declare function to compare user input with computer
 function playRPS(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return 'It\'s a tie!';
-    } else if (playerSelection === 'r' && computerSelection === 's'){
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
         return 'You win!';
-    } else if (playerSelection === 'p' && computerSelection === 'r'){
+    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
         return 'You win!';
-    } else if (playerSelection === 's' && computerSelection === 'p'){
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
         return 'You win!';
     } else {
         return 'You lose!';
