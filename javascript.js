b63c8d21c6a4a938223843f16b2d9294b4ee6c8c
alxfo <120241709+alxfo@users.noreply.github.com>
2023-01-16 22:18:54 +0800

    Add maximum rounds logic and default to 5

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@
 const options = ['rock', 'paper', 'scissors']
 let computerChoice = options[Math.floor(Math.random() * options.length)];
 
+let rounds = 5
+
 // win and loss trackers
 let wins = 0
 let losses = 0
@@ -11,22 +13,36 @@ let losses = 0
 function playRPS(playerSelection, computerSelection) {
     movesDiv.textContent=`${playerSelection} vs. ${computerSelection}!`;
     if (playerSelection === computerSelection) {
-        resultsDiv.textContent='It\'s a tie!';
+        resultsDiv.textContent='The round is tied!';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
         wins += 1;
-        resultsDiv.textContent='You win!';
+        resultsDiv.textContent='You win the round!';
     } else if (playerSelection === 'paper' && computerSelection === 'rock'){
         wins += 1;
-        resultsDiv.textContent='You win!';
+        resultsDiv.textContent='You win the round!';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
         wins += 1;
-        resultsDiv.textContent='You win!';
+        resultsDiv.textContent='You win the round!';
     } else {
         losses += 1;
-        resultsDiv.textContent='You lose!';
+        resultsDiv.textContent='You lose the round!';
     }
+
     winsDiv.textContent=`Wins: ${wins}`
     lossesDiv.textContent=`Losses: ${losses}`
+
+    if (wins === rounds || losses === rounds) {
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+
+        if (wins > losses) {
+            resultsDiv.textContent='YOU WIN'
+        } else {
+            resultsDiv.textContent='YOU LOSE'
+        }
+    }
+
     computerChoice = options[Math.floor(Math.random() * options.length)];
 }
 
@@ -39,6 +55,11 @@ function resetGame() {
     resultsDiv.textContent='Awaiting move';
     winsDiv.textContent='Wins: 0';
     lossesDiv.textContent='Losses: 0';
+
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+
     computerChoice = options[Math.floor(Math.random() * options.length)];
 }
 
