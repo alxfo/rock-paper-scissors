69faf6cceea272c7a831e1f2184e8c3088ee653f
alxfo <120241709+alxfo@users.noreply.github.com>
2023-01-16 22:00:23 +0800

    Add code to place game results onto the page

--- javascript.js
+++ javascript.js
@@ -9,25 +9,24 @@ let computerChoice = options[Math.floor(Math.random() * options.length)];
 
 // function to play a single round and increment the appropriate tracker
 function playRPS(playerSelection, computerSelection) {
-    console.log(`You picked ${playerSelection}`);
-    console.log(`The AI picked ${computerSelection}`)
+    movesDiv.textContent=`${playerSelection} vs. ${computerSelection}!`;
     if (playerSelection === computerSelection) {
-        console.log('It\'s a tie!');
+        resultsDiv.textContent='It\'s a tie!';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
         wins += 1;
-        console.log('You win!');
+        resultsDiv.textContent='You win!';
     } else if (playerSelection === 'paper' && computerSelection === 'rock'){
         wins += 1;
-        console.log('You win!');
+        resultsDiv.textContent='You win!';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
         wins += 1;
-        console.log('You win!');
+        resultsDiv.textContent='You win!';
     } else {
         losses += 1;
-        console.log('You lose!');
+        resultsDiv.textContent='You lose!';
     }
-    console.log(`Wins: ${wins}`)
-    console.log(`Losses: ${losses}`)
+    winsDiv.textContent=`Wins: ${wins}`
+    lossesDiv.textContent=`Losses: ${losses}`
     computerChoice = options[Math.floor(Math.random() * options.length)];
 }
 
@@ -35,7 +34,11 @@ const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
 
-
 rockBtn.addEventListener('click', () => playRPS('rock', computerChoice));
 paperBtn.addEventListener('click', () => playRPS('paper', computerChoice));
 scissorsBtn.addEventListener('click', () => playRPS('scissors', computerChoice));
+
+const movesDiv = document.querySelector('#moves');
+const resultsDiv = document.querySelector('#results');
+const winsDiv = document.querySelector('#wins')
+const lossesDiv = document.querySelector('#losses')
